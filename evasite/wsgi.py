e2cf686b9d51d0558c328d07721c112a646a4229
kaiforward <kaiforward123@gmail.com>
2018-01-19 16:11:08 +0000

    Getting to work on Heroku

--- evasite/wsgi.py
+++ evasite/wsgi.py
@@ -1,16 +1,18 @@
 """
-WSGI config for evasite project.
+WSGI config for project3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+ 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evasite.settings")
-
+from whitenoise.django import DjangoWhiteNoise
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
+ 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
