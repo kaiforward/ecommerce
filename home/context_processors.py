6722ca78f2d9d1b3cfde8eeeef7ae9abad2ee3dc
kaiforward <kaiforward123@gmail.com>
2018-01-19 18:49:20 +0000

    Decimal Precision Error

--- home/context_processors.py
+++ home/context_processors.py
@@ -8,4 +8,4 @@ def nav_homepage(request):
 	"""
     Return Categories for use by Navbar
     """
-	return {'navbar_content': NavbarImage.objects.all()[0]}
+	return {'navbar_content': NavbarImage.objects.all()}
