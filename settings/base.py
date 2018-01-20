1973ab8018fec738183a9a226190c794aa522bf4
kaiforward <kaiforward123@gmail.com>
2018-01-20 01:33:20 +0000

    set correct django  variables

--- settings/base.py
+++ settings/base.py
@@ -17,12 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = []
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e)r$ns-5f-b#+3$u$e%z)=$l7b4zy5x^rk4-s22*11^dl*y^+x'
+SECRET_KEY = os.environ['EVA_SITE_SECRET_KEY']
 
 # Application definition
 
