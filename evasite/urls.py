031df42bfb10bef871f11c94275f358fe5bd98f6
kaiforward <kaiforward123@gmail.com>
2018-01-25 02:39:17 +0000

    Updated requirements

--- evasite/urls.py
+++ evasite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include, re_path
 from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
@@ -33,8 +34,29 @@ urlpatterns = [
 
     # this works i think? new pathign is confusing at the moment
     re_path(r'media/(?P<path>.*)?$', serve, {'document_root': settings.MEDIA_ROOT}),
+    
     path('admin/', admin.site.urls),
 
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
 
 if settings.DEBUG:
