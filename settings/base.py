bfcbbffc86db09ffb70951a9903417c1188e244e
kaiforward <kaiforward123@gmail.com>
2018-01-31 20:23:32 +0000

    moved secret variables to correct place and replaced them

--- settings/base.py
+++ settings/base.py
@@ -142,8 +142,9 @@ COUNTRIES_FIRST = [
 
 AWS_STORAGE_BUCKET_NAME = 'kaiforwardtest'
 AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAJY6QFD7PO33O4MTQ'
-AWS_SECRET_ACCESS_KEY = 'Q6QKPJIY/jUwMkyJ8oF+uhl9mKcnpA6iHiKfIALr' 
+
+AWS_ACCESS_KEY_ID = os.environ['EVA_SITE_AWS_PUBLIC']
+AWS_SECRET_ACCESS_KEY = os.environ['EVA_SITE_AWS_SECRET']
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
